3a28b831647f06497c5268d699c6ae03eedabb62
Wagner Jacó <wagnerjaco@gmail.com>
2026-05-26 09:00:55 -0300

    Clean up ExtencionSecurity.cs by removing unused code
    Removed commented-out code and documentation for security services.

--- elsa-studio-3.6.1/Elsa.Studio/Services/ExtencionSecurity.cs
+++ elsa-studio-3.6.1/Elsa.Studio/Services/ExtencionSecurity.cs
@@ -23,27 +23,3 @@ namespace Elsa.Studio.Services
     }
 }
 
-
-//using Elsa.Studio.Contracts;
-//using Elsa.Studio.Security.Menu;
-//using Microsoft.Extensions.DependencyInjection;
-
-//namespace Elsa.Studio.Security.Extensions;
-
-/// <summary>
-/// Provides extension methods for configuring security services.
-/// </summary>
-//public static class ServiceCollectionExtensions
-//{
-//    /// <summary>
-//    /// Adds the security module services to the service collection.
-//    /// </summary>
-//    /// <param name="services">The service collection to add services to.</param>
-//    /// <returns>The service collection for method chaining.</returns>
-//    public static IServiceCollection AddSecurityModule(this IServiceCollection services)
-//    {
-//        return services
-//            .AddScoped<IFeature, Feature>()
-//            .AddScoped<IMenuProvider, SecurityMenu>();
-//    }
-//}
